d8ae56c88b8c731c650100f9845911f1ccbff3ee
Tim Greene <tim.g.greene@gmail.com>
2020-10-05 18:08:53 +0100

    Remove dom-helpers, add lib/canUseDom
    Remove an unneeded dependency on dom-helpers and simply add a local replacement
    for determining that we can use the dom (or essentially that we are in a
    browser).

--- src/portal/src/Portal.js
+++ src/portal/src/Portal.js
@@ -1,7 +1,7 @@
 import { Component } from 'react'
-import canUseDom from 'dom-helpers'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
+import canUseDom from '../../lib/canUseDom'
 
 let portalContainer
 
